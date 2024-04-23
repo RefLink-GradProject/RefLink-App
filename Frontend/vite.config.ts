839dd3262dcd0d7d6cdf1fdee55518cd4eb05c43
Zoldier777 <Zzer0ph@gmail.com>
2024-04-23 21:00:21 +0200

    config: added https

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: { https: true, port: 5173 },
+  plugins: [react(), mkcert()],
+});
