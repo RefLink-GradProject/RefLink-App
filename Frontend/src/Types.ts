d0f602b306e7ded212b5e97f5161657cd9797737
luoxinnan <luoxinnan1@gmail.com>
2024-04-23 10:26:41 +0200

    Creates types base on backend models

---
+++ Frontend/src/Types.ts
@@ -0,0 +1,33 @@
+
+export type Posting = {
+    guid: number;
+    title: string;
+    description: string;
+    candidates: Candidate[];
+    questions: Question[];
+}
+
+export type Candidate = {
+    guid: number;
+    name: string;
+    email: string;
+    referencers: Referencer[];
+}
+
+export type Referencer = {
+    guid: number;
+    name: string;
+    emial: string;
+    responses: Response[];
+}
+
+export type Response = {
+    guid: number;
+    content: string;
+}
+
+export type Question = {
+    guid: number; 
+    content: string;
+    responses: Response[];
+}
