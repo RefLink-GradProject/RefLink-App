6b216999cccb566c2747c8ae1b5a15764a2a5a66
luoxinnan <luoxinnan1@gmail.com>
2024-04-24 16:42:46 +0200

    Makes Postings responsive

--- Frontend/src/components/Postings.tsx
+++ Frontend/src/components/Postings.tsx
@@ -8,16 +8,16 @@ import PostingDetails from "./PostingDetails";
 export default function Postings({ postings, clickedPosting, setClickedPosting, setClickedCandidate }: Props) {
     return (
         <>
-            <div className='ml-36 mr-36 mt-20 mb-20'>
+            <div className='lg:ml-5 lg:mr-5 xl:ml-32 xl:mr-32  mt-20 mb-20'>
                 <Link to="/postings/add">
                     <button className="btn btn-neutral">Add Posting</button>
                 </Link>
-                <section id="postings" className='w-full flex'>
-                    <div className='w-1/2'>
+                <section id="postings" className='w-full lg:flex'>
+                    <div className='mr-5 lg:w-1/3 '>
                         <PostingTitles postings={postings} clickedPosting={clickedPosting} setClickedPosting={setClickedPosting} />
 
                     </div>
-                    <div className='w-1/2'>
+                    <div className='mt-5 lg:mt-0 lg:w-2/3'>
 
                         <PostingDetails clickedPosting={clickedPosting} setClickedCandidate={setClickedCandidate} />
                     </div>
