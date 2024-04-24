a24940ccdbf2b0fbc67b5f0b81f7c5229eb38516
luoxinnan <luoxinnan1@gmail.com>
2024-04-24 09:48:45 +0200

    Update AddreferencerForm to add several referencers

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { https: true, port: 5173 },
-  plugins: [react(), mkcert()],
-});
+  plugins: [react()],
+})
