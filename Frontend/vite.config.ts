1d5e85cd4f271487fc113eaccb561f0c6b2fb6b3
Pontus <pontus.loefgren@gmail.com>
2024-04-25 16:28:59 +0200

    Sets port to 5000

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {port: 5173, https: true },
+  server: { port: 5000, https: true },
   plugins: [react(), mkcert()],
 });
-
