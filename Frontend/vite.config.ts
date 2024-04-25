ea45873848a5e3a8774d42fc4aabf06e4f329f04
Nan <109028168+luoxinnan@users.noreply.github.com>
2024-04-25 08:58:22 +0200

    Update vite.config.ts to put mkcert back

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {port: 5173 },
-  plugins: [react()],
+  plugins: [react(), mkcert()],
 });
 
