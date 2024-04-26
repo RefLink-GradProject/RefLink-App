c4d5bf2420f973153cb4f70b644bf90c79edbde2
Pontus <pontus.loefgren@gmail.com>
2024-04-26 21:50:11 +0200

    Form gets reviewer and reviewers questions

--- Frontend/src/components/AddReviewForm.tsx
+++ Frontend/src/components/AddReviewForm.tsx
@@ -1,3 +1,4 @@
+import { FieldValues, useForm } from "react-hook-form";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 
@@ -5,11 +6,12 @@ import { useParams } from "react-router-dom";
 export default function AddReviewForm() {
     const { guid } = useParams();
     console.log("guid", guid);
+    const { register, handleSubmit, control } = useForm();
 
     const { isLoading, error, data } = useQuery({
         queryKey: ['getReferencerByGuid'],
         queryFn: async () => {
-            const response = await fetch(`http://localhost:5136/api/referencers/${guid}`, {
+            const response = await fetch(`http://localhost:5136/api/referencers/${guid}/questions`, {
                 headers: {
                     "Content-Type": "application/json",
                 }
@@ -23,9 +25,44 @@ export default function AddReviewForm() {
         }
     });
 
+    async function submitForm(data: FieldValues) {
+        console.log("submitForm", data)
+    }
+
+    if (isLoading) {
+        return (
+            <>
+                <div className="h-full w-full flex justify-center">
+                    <span className="mx-auto loading loading-spinner loading-lg"></span>
+                </div>
+            </>
+        )
+    }
+
+
+    if (error) return 'An error has occurred.'
+
     return (
         <>
-            Hello
+            <h2>Hello {data.referencer.name}!</h2>
+            <p>We appreciate you providing your honest reference.</p>
+            {console.log(data)}
+
+            <form className="w-1/2" onSubmit={handleSubmit(submitForm)}>
+
+                <fieldset className="border border-slate-150 rounded-sm p-3 mb-5">
+                    <legend className="text-sm text-slate-500 mb-2">Add referencers</legend>
+                    {
+                        data.questions.map((question, i) =>
+                            <>
+                                <div key={question.guidId}>
+                                    {question.content}
+                                </div>
+                            </>
+                        )
+                    }
+                </fieldset>
+            </form>
         </>
     )
 
