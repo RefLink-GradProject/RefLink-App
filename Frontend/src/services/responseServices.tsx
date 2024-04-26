73c3910e8f0573f1604a31a80be62cac2c1849ec
luoxinnan <luoxinnan1@gmail.com>
2024-04-26 17:10:01 +0200

    Create postResponse

---
+++ Frontend/src/services/responseServices.tsx
@@ -0,0 +1,12 @@
+export async function postResponse(content: string, questionGuid: string){
+    const response = await fetch("http://localhost:5136/api/responses",{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            content:content,
+            questionGuid: questionGuid
+        })
+    });
+    if(!response.ok)
+        throw new Error("Failed to post new candidate!")
+}
