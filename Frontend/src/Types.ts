0ebf7080c9cc73a5c59794eda2fd084f1fb39722
luoxinnan <luoxinnan1@gmail.com>
2024-04-26 15:18:56 +0200

    Add new types with rating

--- Frontend/src/Types.ts
+++ Frontend/src/Types.ts
@@ -30,6 +30,7 @@ export type ReferencerInCandidateDetails = {
   name: string;
   guidId?: string;
   responses: ResponseWithQuestionContent[];
+  ratings: RatingsWithQuestionContent[];
 };
 
 export type ResponseWithQuestionContent = {
@@ -38,6 +39,13 @@ export type ResponseWithQuestionContent = {
   responseContent: string;
   responseGuidId: string;
 };
+
+export type RatingsWithQuestionContent = {
+  questionContent: string;
+  questionGuidId: string;
+  ratingContent: number;
+  ratingGuidId: string;
+}
 
 export type Referencer = {
   guidId?: string;
