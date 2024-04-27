f1f6af4ff2b6f52bc62639641ea319499a29cbfd
luoxinnan <luoxinnan1@gmail.com>
2024-04-27 19:18:43 +0200

    Display chart in the CandidateDetail page

--- Frontend/src/components/CandidateDetails.tsx
+++ Frontend/src/components/CandidateDetails.tsx
@@ -54,44 +54,69 @@ export default function CandidateDetails({ candidate }: Props) {
 
     return (
         <div className="m-10">
-            <section id="candidate-info">
+            <section id="candidate-info" className="mb-10">
                 <h1 className="text-4xl">{candidate!.name}</h1>
-                <h2 className="text-xl">Email: {candidate!.email}</h2>
-                <h2 className="text-xl">Referencer number: {candidate!.referencers ? candidate!.referencers.length : 0}</h2>
+                <p className="text-">Email: {candidate!.email}</p>
+                <p className="text-">Referencer number: {candidate!.referencers ? candidate!.referencers.length : 0}</p>
             </section>
 
             <section id="candidate-references">
-                {candidate!.referencers && (
-                    candidate!.referencers.map((referencer => {
-                        return (
-                            <>
-                                <div className="collapse collapse-arrow bg-base-200">
-                                    <input type="radio" name="my-accordion-2" defaultChecked />
-                                    <div className="collapse-title text-xl font-medium">
-                                        Reference from {referencer.name}
-                                    </div>
-                                    <div className="collapse-content">
-                                        {referencer.responses!.map((response) => {
-                                            return (
-                                                <>
-                                                    {countWords(response.questionContent) >= 4 && (
-                                                        <>
-                                                            <p className="text-2xl">{response.questionContent}</p>
-                                                            <p>{response.responseContent}</p>
-                                                        </>
-                                                    )
-                                                    }
+                <section id="referenc-texts" className="mb-5">
+                    <h2 className="text-2xl mb-3">Text References:</h2>
+                    {candidate!.referencers && (
+                        candidate!.referencers.map((referencer => {
+                            return (
+                                <>
+                                    <div className="collapse collapse-arrow bg-base-200">
+                                        <input type="radio" name="my-accordion-2" defaultChecked />
+                                        <div className="collapse-title text-xl font-medium">
+                                            <h3 className="text-2xl">Reference from {referencer.name}</h3>
+                                        </div>
+                                        <div className="collapse-content">
+                                            {referencer.responses!.map((response) => {
+                                                return (
+                                                    <>
+                                                        {countWords(response.questionContent) >= 4 && (
+                                                            <>
+                                                                <h4 className="text-xl">{response.questionContent}</h4>
+                                                                <p className="text-sm">{response.responseContent}</p>
+                                                            </>
+                                                        )
+                                                        }
 
-                                                </>
-                                            )
-                                        })}
+                                                    </>
+                                                )
+                                            })}
+                                        </div>
                                     </div>
-                                </div>
-                            </>
-                        )
-                    }))
+                                </>
+                            )
+                        }))
 
-                )}
+                    )}
+                </section>
+
+                <section id="reference-rating" className="mb-5">
+    {candidate!.referencers && (
+        <>
+            <h2 className="text-2xl mb-3">Ratings:</h2>
+            <div className="flex justify-center">
+                <div className="bg-slate-100 w-100 h-50 flex items-center justify-center">
+                    <ResponsiveContainer width={500} height={300}>
+                        <RadarChart outerRadius={100} width={100} height={100} data={getCandidatesRatings()}>
+                            <PolarGrid />
+                            <PolarAngleAxis dataKey="subject" />
+                            <PolarRadiusAxis domain={[0, 5]} />
+                            <Tooltip />
+                            <Radar name="Mike" dataKey="score" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+                        </RadarChart>
+                    </ResponsiveContainer>
+                </div>
+            </div>
+        </>
+    )}
+</section>
+
 
             </section>
             <button className="btn bth-neutral btn-outline btn-sm mr-2 w-20 " onClick={handleBackClik}>&larr; Back</button>
