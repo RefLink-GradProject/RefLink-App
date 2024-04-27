a4762f46191c13b6cb01b7cd2554f699542d9da8
luoxinnan <luoxinnan1@gmail.com>
2024-04-27 14:53:09 +0200

    Add RatingQuestion to Response

--- Backend/refLinkApi/Migrations/20240427125126_AddRatingQuestion.cs
+++ Backend/refLinkApi/Migrations/20240427125126_AddRatingQuestion.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace refLinkApi.Migrations
 {
     /// <inheritdoc />
-    public partial class AddRatingQuestionsToPosting : Migration
+    public partial class AddRatingQuestion : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
