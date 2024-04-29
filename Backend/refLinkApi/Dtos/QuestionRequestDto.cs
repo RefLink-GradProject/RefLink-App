15984ca3e57f8d4da3e3fbe108880a95dce64699
Pontus <pontus.loefgren@gmail.com>
2024-04-29 12:15:13 +0200

    Adds QuestionType

--- Backend/refLinkApi/Dtos/QuestionRequestDto.cs
+++ Backend/refLinkApi/Dtos/QuestionRequestDto.cs
@@ -1,7 +1,10 @@
+using refLinkApi.Models;
+
 namespace refLinkApi.Dtos;
 
 public class QuestionRequestDto
 {
     public required string Content { get; set; }
+    public required QuestionType Type { get; set; }
     public required Guid PostingGuid { get; set; }
 }
