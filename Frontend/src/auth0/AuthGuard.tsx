36649f4448d3023ceb27237704d8e7db407845ca
Patricia van Marlen <patricia.vanmarlen@appliedtechnology.se>
2024-04-30 11:38:02 +0200

    Resolve Git problems

--- Frontend/src/auth0/AuthGuard.tsx
+++ Frontend/src/auth0/AuthGuard.tsx
@@ -1,9 +1,6 @@
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import { CallbackPage } from "./Callback";
-import Register from "../Register";
 import { Navigate } from "react-router-dom";
-import { getEmployerByToken } from "../services/employerService";
-import ChartsDraft from "../components/ChartsDraft";
 
 
 export const AuthGuard = ({ component, employer, auth, ...props }) => {
@@ -12,14 +9,12 @@ export const AuthGuard = ({ component, employer, auth, ...props }) => {
       <CallbackPage />
     ),
   });
-
-  console.log(employer)
-  console.log(auth)
+  
   if (!employer && auth) {
     console.log("why am i hereeee")
     return <Navigate to="/register" replace />
   }
-  else
-    return <Component {...props} />;
+
+  return <Component {...props} />;
 };
 
