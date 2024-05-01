0d1fbef0bfea9d9e5beda1dc1fe916bc85150c9e
luoxinnan <luoxinnan1@gmail.com>
2024-05-01 14:06:15 +0200

    Update default seed data

--- Backend/refLinkApi/Data/SeedData.cs
+++ Backend/refLinkApi/Data/SeedData.cs
@@ -25,8 +25,8 @@ public static class SeedData
             Posting posting = new Posting
             {
                 GuidId = Guid.NewGuid(),
-                Title = "PostingTitle",
-                Description = "PostingDescription",
+                Title = "C# Software Developer",
+                Description = "We're seeking a skilled C# Software Developer to join us, responsible for designing, developing, and maintaining our C# codebase. You'll collaborate with teams, troubleshoot issues, and contribute to our fast-paced, innovative environment.",
                 Employer = employer,
                 EmployerGuid = employer.GuidId,
                 Questions = new List<Question>(),
@@ -37,7 +37,7 @@ public static class SeedData
 
             Question question = new Question()
             {
-                Content = "This is a question",
+                Content = "Has the candidate shown strong teamwork in C# projects?",
                 Posting = posting,
                 Type = QuestionType.Text,
                 PostingGuid = posting.GuidId,
@@ -45,7 +45,7 @@ public static class SeedData
             };
             Question question2 = new Question()
             {
-                Content = "Creativity",
+                Content = "Collaboration",
                 Posting = posting,
                 Type = QuestionType.Rating,
                 PostingGuid = posting.GuidId,
@@ -61,7 +61,7 @@ public static class SeedData
             };
             Question question4 = new Question()
             {
-                Content = "Efficiency",
+                Content = "Creativity",
                 Posting = posting,
                 Type = QuestionType.Rating,
                 PostingGuid = posting.GuidId,
@@ -75,17 +75,7 @@ public static class SeedData
                 PostingGuid = posting.GuidId,
                 Responses = new List<Response>(),
             };
-            
-            Question questionWithoutResponse1 = new Question()
-            {
-                Content = "Social abilities",
-                Posting = posting,
-                Type = QuestionType.Rating,
-                PostingGuid = posting.GuidId,
-                Responses = new List<Response>(),
-            };
-            
-            Question questionWithoutResponse2 = new Question()
+            Question question6 = new Question()
             {
                 Content = "Friendliness",
                 Posting = posting,
@@ -100,18 +90,20 @@ public static class SeedData
             posting.Questions.Add(question3);
             posting.Questions.Add(question4);
             posting.Questions.Add(question5);
+            posting.Questions.Add(question6);
 
             _context.Questions.Add(question);
             _context.Questions.Add(question2);
             _context.Questions.Add(question3);
             _context.Questions.Add(question4);
             _context.Questions.Add(question5);
+            _context.Questions.Add(question6);
             _context.SaveChanges();
 
             Candidate candidate = new Candidate
             {
-                Name = "CandidateName",
-                Email = "CandidateEmail",
+                Name = "Emily Brown",
+                Email = "emily.brown@gmail.com",
                 PostingGuid = posting.GuidId
             };
 
@@ -119,56 +111,72 @@ public static class SeedData
             _context.Candidates.Add(candidate);
             _context.SaveChanges();
 
-            var referencer = new Referencer
+            var referencer1 = new Referencer
             {
-                Name = "Mr. Referencer",
-                Email = "referencer@gmail.com",
+                Name = " Olivia Taylor",
+                Email = "olivia.taylor@mail.com",
+                Responses = new List<Response>(),
+                CandidateGuid = candidate.GuidId,
+            };
+            var referencer2 = new Referencer
+            {
+                Name = " Jessica Smith",
+                Email = " jsmith@mail.com",
                 Responses = new List<Response>(),
                 CandidateGuid = candidate.GuidId,
             };
 
-            _context.Referencers.Add(referencer);
+            _context.Referencers.Add(referencer1);
+            _context.Referencers.Add(referencer2);
             _context.SaveChanges();
 
             var response = new Response()
             {
-                Content = "This is a response",
+                Content = "The candidate actively engages with the team, contributes ideas, and communicates effectively for project success.",
                 Question = question,
-                Referencer = referencer,
+                Referencer = referencer1,
                 QuestionGuid = question.GuidId,
-                ReferencerGuid = referencer.GuidId,
+                ReferencerGuid = referencer1.GuidId,
             };
             var response2 = new Response()
             {
-                Content = "4",
+                Content = "3",
                 Question = question2,
-                Referencer = referencer,
+                Referencer = referencer1,
                 QuestionGuid = question2.GuidId,
-                ReferencerGuid = referencer.GuidId,
+                ReferencerGuid = referencer1.GuidId,
             };
             var response3 = new Response()
             {
-                Content = "2",
+                Content = "4",
                 Question = question3,
-                Referencer = referencer,
+                Referencer = referencer1,
                 QuestionGuid = question3.GuidId,
-                ReferencerGuid = referencer.GuidId,
+                ReferencerGuid = referencer1.GuidId,
             };
             var response4 = new Response()
             {
                 Content = "5",
                 Question = question4,
-                Referencer = referencer,
+                Referencer = referencer1,
                 QuestionGuid = question4.GuidId,
-                ReferencerGuid = referencer.GuidId,
+                ReferencerGuid = referencer1.GuidId,
             };
             var response5 = new Response()
             {
                 Content = "5",
                 Question = question5,
-                Referencer = referencer,
+                Referencer = referencer1,
                 QuestionGuid = question5.GuidId,
-                ReferencerGuid = referencer.GuidId,
+                ReferencerGuid = referencer1.GuidId,
+            };
+            var response6 = new Response()
+            {
+                Content = "1",
+                Question = question6,
+                Referencer = referencer1,
+                QuestionGuid = question6.GuidId,
+                ReferencerGuid = referencer1.GuidId,
             };
 
             _context.Responses.Add(response);
@@ -176,6 +184,7 @@ public static class SeedData
             _context.Responses.Add(response3);
             _context.Responses.Add(response4);
             _context.Responses.Add(response5);
+            _context.Responses.Add(response6);
             _context.SaveChanges();
 
 
