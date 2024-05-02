d15920426c2185e225b8fdd221304fa00d25b7d4
Zoldier777 <Zzer0ph@gmail.com>
2024-05-02 13:27:15 +0200

    change Vite_auth0

--- frontend/src/auth0/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth0/Auth0ProviderWithNavigate.tsx
@@ -4,9 +4,9 @@ import { useNavigate } from "react-router-dom";
 export const Auth0ProviderWithNavigate = ({ children }) => {
   const navigate = useNavigate();
 
-  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
-  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const domain = "dev-5u6q7n8kc0ytrj48.us.auth0.com";
+  const clientId = "FI9oxO12W4vxfL1gh2PG6XXJIgbLj284";
+  const redirectUri = "https://icy-smoke-0b04e3303.5.azurestaticapps.net/callback";
 
   const onRedirectCallback = (appState) => {
     navigate(appState?.returnTo || window.location.pathname);
