20dc62be5df3c6af9ad2969c078cd3dffd626e0e
Patricia van Marlen <patricia.vanmarlen@appliedtechnology.se>
2024-05-02 14:55:03 +0200

    button postingtitles

--- frontend/src/components/PostingTitles.tsx
+++ frontend/src/components/PostingTitles.tsx
@@ -4,8 +4,11 @@ import { Link, useNavigate } from 'react-router-dom';
 
 export default function PostingTitles({ postings }: Props) {
     const [clickedButtons, setClickedButtons] = useState<boolean[]>(Array(postings.length).fill(false));
+    const navigate = useNavigate();
 
-    function handleClick(index: number) {
+    function handleClick(posting: Posting, index: number) {
+        navigate(`/postings/${posting.guidId}`);
+
         const newClickedButtons = Array(postings.length).fill(false);
         newClickedButtons[index] = true;
         setClickedButtons(newClickedButtons);
@@ -19,10 +22,8 @@ export default function PostingTitles({ postings }: Props) {
                         <div key={posting.guidId} className={`flex justify-center ${clickedButtons[index] ? 'bg-neutral-300' : ''}`}>
                             <button
                                 className={`btn btn-ghost btn-wide w-auto text-lg md:text-xl`}
-                                onClick={() => handleClick(index)}
-                            >
-                                <Link to={`/postings/${posting.guidId}`} >{posting.title}</Link>
-                            </button>
+                                onClick={() => handleClick(posting, index)}
+                            >{posting.title}</button>
                         </div>
                     ))}
                 </section>
